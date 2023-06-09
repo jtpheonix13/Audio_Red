3c8fa55f1074eee2eaf00a0a861b9239b7b487f7
jtpheonix13 <jgtihunt@comcast.net>
2023-06-08 21:53:16 -0500

    heroku set up

--- controllers/api/spotifyRoutes.js
+++ controllers/api/spotifyRoutes.js
@@ -10,7 +10,7 @@ const { generateRandomString } = require('../../utils/helpers');
 const spotifyApi = new SpotifyWebApi({
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    redirectUri: 'http://localhost:3001/home/callback' // This should be the callback URL for the authorization flow
+    redirectUri: `${process.env.BASE_URL}/home/callback` // This should be the callback URL for the authorization flow
 });
 
 // Authorization route
