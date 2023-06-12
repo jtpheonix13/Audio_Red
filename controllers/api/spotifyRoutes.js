9960c9bda918837827274f0925d9f1b169f62e37
jtpheonix13 <jgtihunt@comcast.net>
2023-06-11 19:38:04 -0500

    updated spotifyRoutes.js

--- controllers/api/spotifyRoutes.js
+++ controllers/api/spotifyRoutes.js
@@ -5,13 +5,14 @@ const express = require('express');
 const router = express.Router();
 const SpotifyWebApi = require('spotify-web-api-node');
 const { generateRandomString } = require('../../utils/helpers');
+const querystring = require('querystring');
 
-// Set up the Spotify API client
-const spotifyApi = new SpotifyWebApi({
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    redirectUri: `${process.env.BASE_URL}/home/callback` // This should be the callback URL for the authorization flow
-});
+// // Set up the Spotify API client
+// const spotifyApi = new SpotifyWebApi({
+//     clientId: process.env.CLIENT_ID,
+//     clientSecret: process.env.CLIENT_SECRET,
+//     redirectUri: process.env.REDIRECT_URI // This should be the callback URL for the authorization flow
+// });
 
 // Authorization route
 router.get('/login', (req, res) => {
@@ -20,11 +21,19 @@ router.get('/login', (req, res) => {
   // Create the authorization URL
   // console.log(spotifyApi);
   const scopes = ['user-read-private', 'user-read-email', 'user-read-playback-state', 'user-top-read']
-  const authorizeURL = spotifyApi.createAuthorizeURL(scopes, state);
+  //const authorizeURL = spotifyApi.createAuthorizeURL(scopes);
   //console.log(authorizeURL);
   
   // Redirect the user to the authorization URL
-    res.redirect(authorizeURL);
+    res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+    response_type: 'code',
+    client_id: process.env.CLIENT_ID,
+    scope: scopes,
+    redirect_uri: process.env.REDIRECT_URI,
+    show_dialog: true,
+  
+  }));
 });
 
 
