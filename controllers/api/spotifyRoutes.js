c9169da0a9c79f3c4f4d139e747df10b9f22deb2
jtpheonix13 <jgtihunt@comcast.net>
2023-06-11 19:02:49 -0500

    testing

--- controllers/api/spotifyRoutes.js
+++ controllers/api/spotifyRoutes.js
@@ -7,12 +7,12 @@ const SpotifyWebApi = require('spotify-web-api-node');
 const { generateRandomString } = require('../../utils/helpers');
 const querystring = require('querystring');
 
-// Set up the Spotify API client
-const spotifyApi = new SpotifyWebApi({
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    redirectUri: process.env.REDIRECT_URI // This should be the callback URL for the authorization flow
-});
+// // Set up the Spotify API client
+// const spotifyApi = new SpotifyWebApi({
+//     clientId: process.env.CLIENT_ID,
+//     clientSecret: process.env.CLIENT_SECRET,
+//     redirectUri: process.env.REDIRECT_URI // This should be the callback URL for the authorization flow
+// });
 
 // Authorization route
 router.get('/login', (req, res) => {
